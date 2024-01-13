195b6c9b48e482525b109c7416c55cceb367809b
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-13 20:15:51 +0100

    Create Ejercicio2.java

---
+++ Tema1/Aleatorios/Ejercicio2.java
@@ -0,0 +1,22 @@
+package Aleatorios;
+
+import java.util.Random;
+
+public class Ejercicio2 {
+	
+	/* Enunciado:
+	 * 
+	 * El lanzamiento de un dado.
+	 */
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		// Numero aleatorio entre 1 y 6, ambos incluidos
+		int dado = random.nextInt(6 - 1 + 1) + 1;
+		
+		System.out.println(dado);
+
+	}
+
+}
