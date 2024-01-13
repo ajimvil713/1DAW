c5aa270ae85b72a0d1e80352537bdcd966152ccc
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-13 20:20:32 +0100

    Create Ejercicio4.java

---
+++ Tema1/Aleatorios/Ejercicio4.java
@@ -0,0 +1,20 @@
+package Aleatorios;
+
+import java.util.Random;
+
+public class Ejercicio4 {
+	
+	/* Enunciado:
+	 * 
+	 * Un número decimal.
+	 */
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		double numero = random.nextDouble();
+		
+		System.out.println(numero);
+	}
+
+}
