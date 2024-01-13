ca5540a0ac83d830dd8f223395164ee2e70c9bfd
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-13 21:27:29 +0100

    Create Ejercicio3.java

---
+++ Tema1/EscrituraPorTeclado/Ejercicio3.java
@@ -0,0 +1,21 @@
+package EscrituraPorTeclado;
+
+public class Ejercicio3 {
+	
+	/* Enunciado:
+	 * 
+	 * Muestra en consola los siguientes símbolos:
+	 * 1. ❤
+	 * 2. ☂
+	 * 3. ☎
+	 * 4. ♞
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("1. \u2665");
+		System.out.println("2. \u2602");
+		System.out.println("3. \u260E");
+		System.out.println("4. \u265E");
+	}
+
+}
