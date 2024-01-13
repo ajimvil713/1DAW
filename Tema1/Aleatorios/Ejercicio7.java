33a1bd522257b53a47caaf50d40677761fe3bf7a
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-13 20:33:16 +0100

    Create Ejercicio7.java

---
+++ Tema1/Aleatorios/Ejercicio7.java
@@ -0,0 +1,47 @@
+package Aleatorios;
+
+import java.util.Random;
+
+public class Ejercicio7 {
+	
+	/* Enunciado:
+	 * 
+	 * Un día de la semana y mostrar qué día de la semana es.
+	 */
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		int dia = random.nextInt(7 - 1 + 1) + 1;
+		
+		System.out.println("El numero es: " + dia + " y corresponde al");
+		switch (dia) {
+		case 1:
+			System.out.println("Lunes");
+			break;
+		case 2:
+			System.out.println("Martes");
+			break;
+		case 3:
+			System.out.println("Miercoles");
+			break;
+		case 4:
+			System.out.println("Jueves");
+			break;
+		case 5:
+			System.out.println("Viernes");
+			break;
+	case 6:
+			System.out.println("Sábado");
+			break;
+		case 7:
+			System.out.println("Domingo");
+			break;
+		default:
+			System.out.println("Error el numero de la semana no se ha generado correctamente");
+			break;
+		}
+
+	}
+
+}
