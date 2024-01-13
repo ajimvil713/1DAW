846cc03556bbcff551bd51166d1b9df9854c87d5
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-13 20:29:52 +0100

    Create Ejercicio6.java

---
+++ Tema1/Aleatorios/Ejercicio6.java
@@ -0,0 +1,27 @@
+package Aleatorios;
+
+import java.util.Random;
+
+public class Ejercicio6 {
+	
+	/* Enunciado:
+	 * 
+	 * Un mes del año y mostrar si es verano o no 
+   * (entenderemos como verano los meses de julio y agosto).
+	 */
+	
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		int mes = random.nextInt(12 - 1 + 1) + 1;
+		
+		System.out.println("El dia del mes es: " + mes);
+		if (mes == 6 || mes == 7) {
+			System.out.println("Es verano");
+		} else {
+			System.out.println("No es verano");
+		}
+
+	}
+
+}
