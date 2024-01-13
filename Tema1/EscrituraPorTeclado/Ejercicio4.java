64996e6385a9fb2dd05c606bd8192466c3b535e8
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-13 21:36:38 +0100

    Create Ejercicio4.java

---
+++ Tema1/EscrituraPorTeclado/Ejercicio4.java
@@ -0,0 +1,16 @@
+package LecturaPorTeclado;
+
+import static LecturaPorTeclado.colores.*;
+
+public class Ejercicio1 {
+	
+	/* Enunciado:
+	 * 
+	 * Muestra en consola una línea de texto con 3 colores y fondos diferentes usando solamente un printf.
+	 */
+
+	public static void main(String[] args) {
+		System.out.printf(RED + PURPLE_BACKGROUND + "P" + GREEN + CYAN_BACKGROUND + "A" + BLUE + YELLOW_BACKGROUND + "R");
+	}
+
+}
