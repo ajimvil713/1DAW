9a6b210c0f039e9fe52864f6fe4aa3a0c5da9701
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-15 19:29:22 +0100

    Create Ejercicio7.java

---
+++ Tema2/EjerciciosIf/Ejercicio7.java
@@ -0,0 +1,35 @@
+package EjerciciosIf;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+	
+	/* Enunciado:
+	 * 
+	 * Realiza un programa que clasifique un triángulo tras recibir el tamaño de sus lados. Se debe clasificar como triángulo rectángulo, isósceles, equilátero o escaleno. 
+	 */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		// Pedimos los datos al usuario y los almacenamos
+		System.out.print("Introduce el primer lado: ");
+		int lado1 = sc.nextInt();
+		
+		System.out.print("Introduce el segundo lado: ");
+		int lado2 = sc.nextInt();
+		
+		System.out.print("Introduce el tercer lado: ");
+		int lado3 = sc.nextInt();
+		
+		// Comprobamos si es equilátero, isósceles o escaleno
+		if (lado1 == lado2 && lado2 == lado3) {
+			System.out.println("El triangulo es equilatero");
+		} else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3){
+			System.out.println("El triángulo es isósceles");
+		} else {
+			System.out.println("El triángulo es escaleno");
+		}
+	}
+
+}
