e784a03ca54c7cd0bb462bf35d358df5cb511751
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-15 18:17:43 +0100

    Update Eejercicio1.java

--- Tema2/EjerciciosRepaso/Eejercicio1.java
+++ Tema2/EjerciciosRepaso/Eejercicio1.java
@@ -1,1 +1,36 @@
-//
+package EjerciciosRepaso;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+  
+	/* Enunciado:
+	 * 
+	 * Realiza una función que lea una cadena por teclado y devuelva si es tiene una longitud de más de seis cifras.
+	 */
+  
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		// Declaramos las variables
+		String cadena;
+		
+		// Pedimos datos al usuario
+		System.out.println("Introduce la cadena a analizar");
+		cadena = sc.nextLine();
+		
+		// Enviamos cadena a la función
+		leerCadena(cadena);
+	}
+	
+	// Función que devuelve si la cadena tiene más de 6 cifras
+	public static void leerCadena(String cadena) {
+		
+		if (cadena.length() >= 6) {
+			System.out.println("La longitud de la cadena es mayor o igual a 6 caracteres");
+		} else {
+			System.out.println("La longitud de la cadena es menor de 6 caracteres");
+		}
+	}
+  
+}
