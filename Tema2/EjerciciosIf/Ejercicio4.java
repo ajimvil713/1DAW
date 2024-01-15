e1ffc229e1a3bf490bf6b39b08e0b604cec81264
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-15 19:12:30 +0100

    Update Ejercicio4.java

--- Tema2/EjerciciosIf/Ejercicio4.java
+++ Tema2/EjerciciosIf/Ejercicio4.java
@@ -7,7 +7,6 @@ public class Ejercicio4 {
 	/* Enunciado:
 	 * 
 	 * Realiza un programa que lea una cadena por teclado y compruebe si es una letra mayúscula.
-	 *  
 	 */
 
 	public static void main(String[] args) {
