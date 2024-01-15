39d013eea0ce7e4e97ceda65a78a3f3f2728f39d
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-15 18:59:12 +0100

    Create Ejercicio1.java

---
+++ Tema2/EjerciciosIf/Ejercicio1.java
@@ -0,0 +1,33 @@
+package EjerciciosIf;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	
+	/* Enunciado:
+	 * 
+	 * Realiza un programa que reciba dos números por teclado e indique cuál es mayor o si son iguales. 
+	 */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		// Pedimos los datos al usuario y los almacenamos
+		System.out.print("Introduce un número: ");
+		int numero1 = sc.nextInt();
+		
+		System.out.print("Introduce otro numero: ");
+		int numero2 = sc.nextInt();
+		
+		// Miramos cual es el mayor
+		if (numero1 > numero2) {
+			System.out.println(numero1 + " es mayor que " + numero2);
+		} else if (numero1 < numero2){
+			System.out.println(numero2 + " es mayor que " + numero1);
+		} else {
+			System.out.println("Has introducido 2 numeros iguales");
+		}
+
+	}
+
+}
