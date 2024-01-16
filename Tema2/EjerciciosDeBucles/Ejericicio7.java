26d1660cdcc232983d8700addffcb19cba390dbc
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-16 16:55:57 +0100

    Create Ejericicio7.java

---
+++ Tema2/EjerciciosDeBucles/Ejericicio7.java
@@ -0,0 +1,32 @@
+package EjerciciosDeBucles;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+	
+	/* Enunciado:
+	 * 
+	 * Algoritmo que dado un número long, devuelva cuántas cifras tiene. (no usar String).
+	 */
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		// Pedimos los datos al usuario y los almacenamos
+		System.out.println("Introduce un número tipo long");
+		long numero = sc.nextLong();
+		
+		int contador = 0;
+		
+		if (numero == 0) {
+			System.out.println("El numero tiene 1 cifra");;
+		} else {
+			while (numero != 0) {
+				numero = numero / 10;
+				contador++;
+			}
+			System.out.println("El numero tiene " + contador + " cifras.");
+		}
+	}
+
+}
