65117824249305c18395c102cc225f73fa52fde7
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 16:49:38 +0100

    Update Ejercicio1.java

--- Tema1/EscrituraPorTeclado/Ejercicio1.java
+++ Tema1/EscrituraPorTeclado/Ejercicio1.java
@@ -1,14 +1,6 @@
 package EscrituraPorTeclado;
 
 public class Ejercicio1 {
-	
-	/* Enunciado:
-	 * 
-	 * Muestra en consola lo siguiente, exactamente igual, utilizando System.out.print:
-	 * La letra número "101" es la :        'e'
-	 *     El carácter '\' es:
-	 *           "especial"
-	 */
 
 	public static void main(String[] args) {
 		System.out.print("La letra número \"101\" es la: \t 'e' \n");
