277e1cc798c16f28d13d56b16650e84921a21583
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 17:30:14 +0100

    Update Ejercicio6.java

--- Tema2/EjerciciosIf/Ejercicio6.java
+++ Tema2/EjerciciosIf/Ejercicio6.java
@@ -3,14 +3,6 @@ package EjerciciosIf;
 import java.util.Scanner;
 
 public class Ejercicio6 {
-	
-	/* Enunciado:
-	 * 
-	 * Realiza un programa que calcule la aceptación de una solicitud en base a los siguientes parámetros: edad, nota y sexo. 
-	 * Mínimo: Nota (5), edad (18), sexo M -> POSIBLE 
-	 * Mínimo: Nota (5), edad (18), sexo F -> ACEPTADA 
-	 * Otros casos -> NO ACEPTADA 
-	 */
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
