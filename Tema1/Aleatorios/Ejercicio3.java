2bec97abeb2492a5394baafa28b5a3a4ded908f5
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 16:44:39 +0100

    Update Ejercicio3.java

--- Tema1/Aleatorios/Ejercicio3.java
+++ Tema1/Aleatorios/Ejercicio3.java
@@ -4,11 +4,6 @@ import java.util.Random;
 
 public class Ejercicio3 {
 	
-	/* Enunciado:
-	 * 
-	 * Un número entre 34 y 68 ambos incluidos.
-	 */
-	
 	public static void main(String[] args) {
 		Random random = new Random();
 		
