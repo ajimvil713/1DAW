bc671c934ed380586ae1dde1f8c5fe7075d532ea
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 18:22:00 +0100

    Update Ejercicio2.java

--- Tema2/EjerciciosDeBucles/Ejercicio2.java
+++ Tema2/EjerciciosDeBucles/Ejercicio2.java
@@ -1,11 +1,6 @@
 package EjerciciosDeBucles;
 
 public class Ejercicio2 {
-	
-	/* Enunciado:
-	 * 
-	 * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
-	 */
 
 	public static void main(String[] args) {
 		int numero = 0;
