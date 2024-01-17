3038b8ea0b0d6371c5e824f4525f5be4a14f1415
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 17:28:31 +0100

    Update Ejercicio10.java

--- Tema2/EjerciciosIf/Ejercicio10.java
+++ Tema2/EjerciciosIf/Ejercicio10.java
@@ -3,14 +3,6 @@ package EjerciciosIf;
 import java.util.Scanner;
 
 public class Ejercicio10 {
-	
-	/* Enunciado:
-	 * 
-	 * El director de una escuela está organizando un viaje de estudios y requiere determinar cuánto debe cobrar a cada alumno y cuánto debe pagar a la compañía de viajes por el servicio. La forma de cobrar es la siguiente: 
-	 * Si son 100 alumnos o más, el costo por cada alumno es de 65 euros. * De 50 a 99 alumnos, el costo es de 70 euros.  
-	 * De 30 a 49 alumnos, el costo es de 95 euros. 
-	 * Menos de 30 alumnos, el costo de la renta del autobús es de 4000 euros, sin importar el número de alumnos. Realiza un algoritmo que permita determinar el pago a la compañía de autobuses y lo que debe pagar cada alumno por el viaje. 
-	 */
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
