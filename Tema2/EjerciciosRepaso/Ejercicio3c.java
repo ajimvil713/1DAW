f908f23a8d812c3fe2aba6c881b40e918d1b59d2
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 17:22:39 +0100

    Update Ejercicio3c.java

--- Tema2/EjerciciosRepaso/Ejercicio3c.java
+++ Tema2/EjerciciosRepaso/Ejercicio3c.java
@@ -4,11 +4,6 @@ import java.util.Scanner;
 
 public class Ejercicio3c {
 	
-	/* Enunciado:
-	 * 
-	 * Realiza el mismo ejercicio indicando si algún número es menor que 10
-	 */
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
