073a1e3b6843e57a6bdd24e51b4028f44739ba02
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 17:30:54 +0100

    Update Ejercicio9.java

--- Tema2/EjerciciosIf/Ejercicio9.java
+++ Tema2/EjerciciosIf/Ejercicio9.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 
 public class Ejercicio9 {
 	
-	/* Enunciado:
-	 * 
-	 * La asociación de vinicultores tiene como política fijar un precio inicial al kilo de uva, la cual se clasifica en tipos (A y B), y además en tamaños (1 y 2). Cuando se realiza la venta del producto, ésta es de un sólo tipo y tamaño, se requiere determinar cuanto recibirá un productor por la uva que entrega en un embarque considerando lo siguiente:
-	 * Si es de tipo A, se le cargan 20 céntimos al precio inicial cuando es de tamaño 1 y 30 céntimos si es de tamaño 2. 
-	 * Si es de tipo B, se rebajan 30 céntimos cuando es de tamaño 1, y 50 céntimos cuando es de tamaño 2. 
-	 */
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
