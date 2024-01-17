5742a8ccdc168a053203aec9ec93c082d8d07be6
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 16:51:20 +0100

    Update Ejercicio2.java

--- Tema1/EscrituraPorTeclado/Ejercicio2.java
+++ Tema1/EscrituraPorTeclado/Ejercicio2.java
@@ -1,30 +1,6 @@
 package EscrituraPorTeclado;
 
 public class Ejercicio2 {
-	
-	/* Enunciado:
-	 * 
-	 * Utilizando las siguientes variables:
-	 * int x = 10;
-	 * int y = -10;
-	 * float n = 13.269834f;
-	 * String cad = “Ana”;
-	 *
-	 * Muestra en consola el siguiente resultado, exactamente igual,
-	 * utilizando System.out.printf , sin utilizar espacios ni tabulación
-	 * en la cadena de formato y utilizando solamente las variables indicadas
-	 * anteriormente en los argumentos:
-	 * 
-	 * 10
-	 * +10
-	 * -10
-	 * 13,27
-	 *   +13,2698
-	 *   13,26983
-	 * +00013,270
-	 * n=13,27   x=10
-	 *       AnaAna  Ana
-	 */
 
 	public static void main(String[] args) {
 		// Declaramos las variables del ejercicio
