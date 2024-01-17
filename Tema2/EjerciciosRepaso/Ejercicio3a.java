3e6e78a257828b6ff01ad5bb7ccb4b4c7d13ea55
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-17 17:22:04 +0100

    Update Ejercicio3a.java

--- Tema2/EjerciciosRepaso/Ejercicio3a.java
+++ Tema2/EjerciciosRepaso/Ejercicio3a.java
@@ -5,14 +5,6 @@ import static EjerciciosRepaso.ClaseColores.*;
 
 public class Ejercicio3a {
 	
-	/* Enunciado:
-	 * 
-	 * Realiza un procedimiento que pida un número por teclado e imprima las unidades y las decenas, de la siguiente manera: (hacerlo con printf)
-	 * **numero 25**
-	 * 
-	 * Unidades = 5                 Decenas = 2
-	 */
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
@@ -26,10 +18,10 @@ public class Ejercicio3a {
 	
 	// Función que separa las unidades y pinta las unidades con resaltado amarillo y las decenas de color amarillo
 	public static void separarUnidades(int numero) {
-    // Ponemos la primera linea en negrita
+    		// Ponemos la primera linea en negrita
 		System.out.printf(BOLD + "Numero: %d \n" + RESET, numero);
     
-    // Fondo amarillo a las unidades y letras amarillas a las decenas
+   		 // Fondo amarillo a las unidades y letras amarillas a las decenas
 		System.out.printf(YELLOW_BACKGROUND + "Unidades: %d" + RESET + "                 " + YELLOW + "Decenas: %d" + RESET, numero % 10, (numero / 10) % 10);
 	}
   
