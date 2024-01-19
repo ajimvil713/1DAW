b47b62844b15f7dbc9c8d6a8cf19d01bb488d1dc
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-01-19 18:49:54 +0100

    Create Animales.java

---
+++ Tema7/EjemploAnimales/Animales.java
@@ -0,0 +1,40 @@
+package Animales;
+
+public class EjemploAnimales {
+	
+	protected String nombre;
+	protected int velocidad;
+	protected int edad;
+	
+	public Animales(String nombre, int velocidad, int edad) {
+		this.nombre = nombre;
+		this.velocidad = velocidad;
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Animales [nombre=" + nombre + ", velocidad=" + velocidad + ", edad=" + edad + "]";
+	}
+	
+	public int calcularMetros(int segundos) {
+		return this.velocidad * segundos;
+	}
+	
+}
