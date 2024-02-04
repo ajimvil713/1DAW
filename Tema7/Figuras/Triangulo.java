06dfd6727b2b4aba60e8bdcd4fa974739b43580c
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-02-04 18:12:11 +0100

    Update Triangulo.java

--- Tema7/Figuras/Triangulo.java
+++ Tema7/Figuras/Triangulo.java
@@ -1,56 +1,59 @@
 package Figuras;
 
-public class ProgramaFiguras {
+public class Triangulo implements iFigura2D {
+	
+	private double lado, altura;
+	
+	// Constructor
+	public Triangulo(double lado, double altura) {
+		super();
+		this.lado = lado;
+		this.altura = altura;
+	}
+	
+	// Getters y setters
+	public double getLado() {
+		return lado;
+	}
 
-	public static void main(String[] args) {
-		// 1º Creamos el array (No se hace con arrayList porque no se ha visto todavia en clase)
-		iFigura2D[] figuras = new iFigura2D[5];
-		
-		// 2º Añadimos figuras al arrayList
-		figuras[0] = new Cuadrado(4.0);
-	    figuras[1] = new Rectangulo(3.0, 5.0);
-	    figuras[2] = new Triangulo(4.0, 6.0);
-	    figuras[3] = new Circulo(2.0);
-	    
-	    // 3º Mostramos la informacion de las figuras
-	    System.out.println("Informacion de las figuras");
-	    for (iFigura2D figura : figuras) {
-			if (figura != null) {
-				figura.imprimir();
-			}
-		}
-	    System.out.println();
-	    
-	    // 4º Escalamos las figuras a 2
-	    for (iFigura2D figura : figuras) {
-			if (figura != null) {
-				figura.escalar(2);
-			}
-		}
-	    
-	    // 5º Mostrsamos la informacion despues de escalar a 2
-	    System.out.println("Informacion de las figuras despues de escalar a 2");
-	    for (iFigura2D figura : figuras) {
-			if (figura != null) {
-				figura.imprimir();
-			}
-		}
-	    System.out.println();
-	    
-	    // 6º Escalamos las figuras a 0.1
-	    for (iFigura2D figura : figuras) {
-			if (figura != null) {
-				figura.escalar(0.1);
-			}
-		}
-	    
-	    // 6º Mostramos la información despues de escalar a 0.1
-	    System.out.println("Informacion de las figuras despues de escalar a 0.1");
-	    for (iFigura2D figura : figuras) {
-			if (figura != null) {
-				figura.imprimir();
-			}
+	public void setLado(double lado) {
+		this.lado = lado;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	// Metodos de la interface
+	@Override
+	public double perimetro() {
+		return lado * 3;
+	}
+
+	@Override
+	public double area() {
+		return lado * altura / 2;
+	}
+
+	@Override
+	public void escalar(double escala) {
+		if (escala > 0) {
+			lado = lado * escala;
+			altura = altura * escala;
 		}
 	}
 
+	@Override
+	public void imprimir() {
+		System.out.println("--- Triangulo (Equilatero) ---");
+		System.out.println("lado: " + lado);	
+		System.out.println("Altura: " + altura);
+		System.out.println("Perimetro: " + perimetro());
+		System.out.println("Area: " + area());
+	}
+	
 }
