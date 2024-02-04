9536b0e95a91bcdd79a3178782e7e71716aba2f7
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-02-04 12:49:45 +0100

    Create Loro.java

---
+++ Tema7/ClasesAbstractaseInterfaces/Loro.java
@@ -0,0 +1,46 @@
+package Animales;
+
+public class Loro extends Aves {
+	
+	private String origen;
+	private String saluda = "hola";
+	
+	// Constructor
+	public Loro(String nombre, int edad, int fechaNacimiento, String origen, String saluda) {
+		super(nombre, edad, fechaNacimiento, "Pico curvo", true);
+		this.origen = origen;
+		this.saluda = saluda;
+	}
+	
+	// Metodos getters y setters
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getSaluda() {
+		return saluda;
+	}
+
+	public void setSaluda(String saluda) {
+		this.saluda = saluda;
+	}
+	
+	// Metodos para imprimir información por pantalla
+	@Override
+	public String toString() {
+		return super.toString() + ", Loro [origen=" + origen + ", saluda=" + saluda + "]";
+	}
+	
+	public void habla() {
+		System.out.println(saluda);
+	}
+	
+	public void muestra() {
+		System.out.println(super.toString());
+	}
+	
+}
