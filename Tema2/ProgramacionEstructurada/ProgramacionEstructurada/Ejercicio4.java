dfbb5e33491c1660083333832cff7ad09dc79124
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-02-05 19:16:27 +0100

    Update Ejercicio4.java

--- Tema2/ProgramacionEstructurada/ProgramacionEstructurada/Ejercicio4.java
+++ Tema2/ProgramacionEstructurada/ProgramacionEstructurada/Ejercicio4.java
@@ -1,21 +1,23 @@
 package ProgramacionEstructurada;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-	  int numero;
-	
-	  System.out.println("Introduce un número");
-	  numero = sc.nextInt();
-	
-	  if (numero > 0) {
-		  System.out.println("El numero es positivo");
-	  } else if(numero < 0) {
-		  System.out.println("El numero es negativo");
-	  } else {
-		  System.out.println("El numero es 0");
-	  }
+		int numero;
+		
+		System.out.println("Introduce un número");
+		numero = sc.nextInt();
+		
+		if (numero > 0) {
+			System.out.println("El numero es positivo");
+		} else if(numero < 0) {
+			System.out.println("El numero es negativo");
+		} else {
+			System.out.println("El numero es 0");
+		}
 	}
 
 }
