c6da75d966f5b9f97c7f2e7cde034bb6897b564d
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-02-05 19:10:10 +0100

    Create Ejercicio2.java

---
+++ Tema2/ProgramacionEstructurada/ProgramacionEstructurada/Ejercicio2.java
@@ -0,0 +1,13 @@
+package ProgramacionEstructurada;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		int numero = 10;
+	
+	if (numero % 2 == 0) {
+		System.out.println("El numero es par");
+	} else {
+		System.out.println("El numero es impar");
+	}
+}
