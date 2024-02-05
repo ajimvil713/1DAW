846dd9aa14382194883ac7a1081ef1a48bcc97ab
ajimvil713 <156540387+ajimvil713@users.noreply.github.com>
2024-02-05 12:18:37 +0100

    Create Persona.java

---
+++ Tema7/EjemploCompareTo/Persona.java
@@ -0,0 +1,39 @@
+package EjemploCompareTo;
+
+public class Persona implements Comparable {
+	
+	private String nombre, apellidos;
+	private int edad, antiguedad;
+	
+	// Constructor
+	public Persona(String nombre, String apellidos, int edad, int antiguedad) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.edad = edad;
+		this.antiguedad = antiguedad;
+	}
+	
+	// Metodo toString
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", apellidos=" + apellidos + ", edad=" + edad + ", antiguedad="
+				+ antiguedad + "]";
+	}
+	
+	// Metodos de la interface Comparable
+	@Override
+	public int compareTo(Object o) {
+		// Realizamos el casting, ya que trabajamos con la clase Object
+		Persona p = (Persona) o;
+		
+		// Ordenamos por el criterio de ordenacion que queramos
+		// En este caso si las edades son iguales por antiguedad y si no edad
+		if (p.edad == this.edad) {
+			return p.antiguedad - this.antiguedad;
+		} else {
+			return p.edad - this.edad;
+		}
+	}
+	
+}
